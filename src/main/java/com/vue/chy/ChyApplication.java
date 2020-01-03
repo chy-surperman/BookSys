ddedb0d348c75c9519dd95931abc5f072cdb406a
chy <chy>
2020-01-03 09:41:30 +0800

    初始化

---
+++ src/main/java/com/vue/chy/ChyApplication.java
@@ -0,0 +1,13 @@
+package com.vue.chy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChyApplication.class, args);
+    }
+
+}
