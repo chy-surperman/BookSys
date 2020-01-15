0d68aaba2ecb358803affa7f87f88762b9399dac
chy <chy>
2020-01-15 10:31:48 +0800

    ---后台初始化

---
+++ src/test/java/com/vue/chy/ChyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vue.chy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
